f79aad8d0cf21ee9843a4c99ef6867cdfcf3990d
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-30 23:54:16 -0300

    feat: Add unit test for AnswerQuestionUseCase

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import { expect, test } from "vitest";
 import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswersRepository } from "../repositories/answers-repository";
+import { Answer } from "../entities/answer";
 
-test("create an answer", () => {
-  const answerQuestion = new AnswerQuestionUseCase();
+const fakeANswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  },
+};
 
-  const answer = answerQuestion.execute({
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeANswersRepository);
+
+  const answer = await answerQuestion.execute({
     questionId: "1",
     instructorId: "1",
     content: "Nova resposta",
