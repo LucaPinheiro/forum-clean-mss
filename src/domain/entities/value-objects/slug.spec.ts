ccbf50e2855495fcd832928560e4571789c15017
LucaPinheiro <lucapgomes11@gmail.com>
2024-09-23 00:20:13 -0300

    Refactor import paths to use relative paths with @ alias

--- src/domain/entities/value-objects/slug.spec.ts
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -1,7 +1,7 @@
-import { expect, test } from "vitest";
-import { Slug } from "./slug";
+import { expect, test } from 'vitest'
+import { Slug } from './slug'
 
-test("it should be able to create a new slug from a text", () => {
-  const slug = Slug.createFromText("An example title");
-  expect(slug.value).toBe("an-example-title");
-});
+test('it should be able to create a new slug from a text', () => {
+  const slug = Slug.createFromText('An example title')
+  expect(slug.value).toBe('an-example-title')
+})
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,21 +1,19 @@
-import { expect, test } from "vitest";
-import { AnswerQuestionUseCase } from "./answer-question";
-import { AnswersRepository } from "../repositories/answers-repository";
-import { Answer } from "../entities/answer";
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
 
 const fakeANswersRepository: AnswersRepository = {
-  create: async (answer: Answer) => {
-    return;
-  },
-};
+  create: async (answer: Answer) => {},
+}
 
-test("create an answer", async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeANswersRepository);
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeANswersRepository)
 
   const answer = await answerQuestion.execute({
-    questionId: "1",
-    instructorId: "1",
-    content: "Nova resposta",
-  });
-  expect(answer.content).toEqual("Nova resposta");
-});
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova resposta',
+  })
+  expect(answer.content).toEqual('Nova resposta')
+})
