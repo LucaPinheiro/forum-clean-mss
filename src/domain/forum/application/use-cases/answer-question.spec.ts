1bad63b04f6da4a72691bec9a0bf4dd2389c55b4
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-06 16:45:06 -0300

    Refactor import paths in answer-question.spec.ts, answer-question.ts, and answer.ts

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -3,13 +3,13 @@ import { AnswerQuestionUseCase } from './answer-question'
 import { Answer } from '../../enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
 
-const fakeANswersRepository: AnswersRepository = {
+const fakeAnswersRepository: AnswersRepository = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   create: async (answer: Answer) => {},
 }
 
 test('create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeANswersRepository)
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
   const answer = await answerQuestion.execute({
     questionId: '1',
