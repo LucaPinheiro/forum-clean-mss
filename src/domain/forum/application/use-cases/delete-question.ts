da4b32f36b3a629486a99985d5ecf1ce86112797
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-26 00:55:35 -0300

    Refactor delete-question.ts to include use case for deleting a question

---
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -0,0 +1,24 @@
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+
+interface DeleteQuestionUseCaseRequest {
+  authorId: string;
+  questionId: string;
+}
+interface DeleteQuestionUseCaseResponse {}
+export class DeleteQuestionUseCase {
+  constructor(private questionsRepository: InMemoryQuestionsRepository) {}
+  async execute({
+    questionId,
+    authorId,
+  }: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId);
+    if (!question) {
+      throw new Error("Question not found.");
+    }   
+    if (authorId !== question.authorId.toString()) {
+      throw new Error("Not allowed.");
+    }
+    await this.questionsRepository.delete(question);
+    return {};
+  }
+}
