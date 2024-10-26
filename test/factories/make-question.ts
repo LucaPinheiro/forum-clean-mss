2b48cee98ab8f50d472f5e98e1564a1a1098d96a
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-26 00:08:25 -0300

    Refactor makeQuestion factory function to include override parameter

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,3 +1,17 @@
-export function makeQuestion() {
-    
+import { UniqueEntityID } from "../../src/core/entities/unique-entity-id";
+import { Question, QuestionProps } from "../../src/domain/forum/enterprise/entities/question";
+import { Slug } from "../../src/domain/forum/enterprise/entities/value-objects/slug";
+
+
+export function makeQuestion(
+    override: Partial<QuestionProps> = {}
+) {
+  const question = Question.create({
+    authorId: new UniqueEntityID(),
+    title: "Example test",
+    slug: Slug.create("example-question"),
+    content: "Example content",
+  });
+
+  return question
 }
