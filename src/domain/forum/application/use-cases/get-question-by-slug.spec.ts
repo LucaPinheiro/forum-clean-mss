bf63dfecc72fd99da2a3000f31b6ad0213cb39e1
LucaPinheiro <lucapgomes11@gmail.com>
2024-10-26 00:20:35 -0300

    Refactor get-question-by-slug.spec.ts to include test for retrieving a question by slug

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -13,17 +13,19 @@ describe("Get Question By Slug", () => {
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
     sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository);
   });
+
   it("should be able to get a question by slug", async () => {
     const newQuestion = makeQuestion({
-        slug: Slug.create('example-question')
-    })
-    
-    inMemoryQuestionsRepository.create(newQuestion)
+      slug: Slug.create("example-question"),
+    });
+
+    await inMemoryQuestionsRepository.create(newQuestion);
 
     const { question } = await sut.execute({
-        slug: 'example-question'
+      slug: "example-question",
     });
-    
-    expect(question.id).toBeTruthy()
+
+    expect(question.id).toBeTruthy();
+    expect(question.title).toEqual(newQuestion.title);
   });
 });
