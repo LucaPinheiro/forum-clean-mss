bf98da2475ba9d7413e3def0ccb0c3a8f5a35dbd
lucapinheiro <lucapgomes11@gmail.com>
2024-11-03 18:20:32 -0300

    Add QuestionCommentsRepository interface for creating question comments

---
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -0,0 +1,4 @@
+import { QuestionComment } from "../../enterprise/entities/question-comment";
+export interface QuestionCommentsRepository {
+  create(questionComment: QuestionComment): Promise<void>;
+}
