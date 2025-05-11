31309ce6cc6b9b7c8382303c167368e013829e11
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-11 12:15:15 -0300

    Implement DeleteQuestionComment use case for managing comment deletions

---
+++ src/domain/forum/application/use-cases/delete-question-comment.ts
@@ -0,0 +1,31 @@
+import { Either, left, right } from "../../../../core/either";
+import { QuestionCommentsRepository } from "../repositories/question-comments-repository";
+
+interface DeleteQuestionCommentUseCaseRequest {
+  authorId: string;
+  questionCommentId: string;
+}
+
+type DeleteQuestionCommentUseCaseResponse = Either<string, {}>;
+
+export class DeleteQuestionCommentUseCase {
+  constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
+  async execute({
+    authorId,
+    questionCommentId,
+  }: DeleteQuestionCommentUseCaseRequest): Promise<DeleteQuestionCommentUseCaseResponse> {
+    const questionComment =
+      await this.questionCommentsRepository.findById(questionCommentId);
+
+    if (!questionComment) {
+      return left("Question comment not found.");
+    }
+
+    if (questionComment.authorId.toString() !== authorId) {
+      throw new Error("You are not the author of this comment.");
+    }
+
+    await this.questionCommentsRepository.delete(questionComment);
+    return right({});
+  }
+}
