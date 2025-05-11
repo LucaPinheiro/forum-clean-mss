031fc8011c0396c333404d2a504bca1e55929853
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-11 12:25:02 -0300

    Refactor InMemoryQuestionCommentsRepository to improve delete and findById methods

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -7,4 +7,22 @@ export class InMemoryQuestionCommentsRepository
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment);
   }
+
+  async delete(questionComment: QuestionComment) {
+    const questionCommentIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    );
+
+    this.items.splice(questionCommentIndex, 1);
+  }
+
+  async findById(id: string): Promise<QuestionComment | null> {
+    const questionComment = this.items.find((item) => item.id.toString() === id);
+
+    if (!questionComment) {
+      return null;
+    }
+
+    return questionComment;
+  }
 }
