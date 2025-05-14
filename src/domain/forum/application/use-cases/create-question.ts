96e39e2cab0e166b5a3550abaa381a61c53a5d8a
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-13 21:17:52 -0300

    Add support for attachments in CreateQuestionUseCase

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -1,11 +1,13 @@
 import { UniqueEntityID } from "../../../../core/entities/unique-entity-id";
 import { Question } from "../../enterprise/entities/question";
+import { QuestionAttachment } from "../../enterprise/entities/question-attachment";
 import { QuestionRepository } from "../repositories/questions-repository";
 
 interface CreateQuestionUseCaseRequest {
   authorId: string;
   title: string;
   content: string;
+  attachmentsIds?: string[];
 }
 
 interface CreateQuestionUseCaseResponse {
@@ -19,12 +21,24 @@ export class CreateQuestionUseCase {
     authorId,
     title,
     content,
+    attachmentsIds = [],
   }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
+
     const question = Question.create({
       authorId: new UniqueEntityID(authorId),
       title,
       content,
     });
+    
+    const questionAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId).toString(),
+        questionId: question.id.toString(),
+      })
+    })
+
+    question.attachments = questionAttachments
+
 
     await this.questionRepository.create(question);
 
